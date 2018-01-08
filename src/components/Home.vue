1aad1f903897ff8d6d1037098e3353c8a22d721a
Bastien <bastien.nicoud@gmail.com>
2018-01-08 13:43:28 +0100

    bas component

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Intranet CPNV'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.home {
+  background-color: bisque;
+}
+
+</style>
